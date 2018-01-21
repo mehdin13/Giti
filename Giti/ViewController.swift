1d1a65732256ce314a2f254bbb5dbf054ba970c1
Mehdi Negahban <mehdi.n13@gmail.com>
2018-01-21 13:24:28 +0330

    log message for output add button to mainstoryboard

--- Giti/ViewController.swift
+++ Giti/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func ButtonClick(_sender:UIButton)
+    {
+     print("this is it example")
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
